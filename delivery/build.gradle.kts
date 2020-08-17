8fb0041327e8b2748352c183eeb64d33f8b352af
jorgecabrera-pedidosya <jorge.cabrera@pedidosya.com>
2020-08-17 10:52:24 -0300

    delivery build gradle fixed

---
+++ delivery/build.gradle.kts
@@ -0,0 +1,59 @@
+val jacksonVersion = "2.11.0"
+
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-noarg:1.3.71")
+    }
+}
+
+plugins {
+    id("org.springframework.boot") version "2.3.1.RELEASE"
+    id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    kotlin("plugin.spring") version "1.3.72"
+    application
+    kotlin("jvm")
+    kotlin("plugin.jpa") version "1.2.71"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(project(":core"))
+    implementation(project(":infrastructure"))
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${jacksonVersion}")
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    // DynamoDB
+    implementation("com.github.derjust:spring-data-dynamodb:5.1.0")
+
+    // MySql
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("mysql:mysql-connector-java")
+
+    // Spring test
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+application {
+    mainClassName = "com.demo.dynamopoc.delivery.DynamoPocApplicationKt"
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
